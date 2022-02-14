dcdb10e22352a8a2a4dc2a78669369a44ca700d0
Oğuzhan Kalelioğlu <oguzhankalelioglu@protonmail.com>
2022-02-14 23:56:12 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-for="item in posts.data" :key="item.id">
+      <pre
+        >       {{ item.attributes.Header }}
+                {{ item.attributes.creator }}
+                {{item.attributes.customers.data[0].attributes.CustomerName}}
+            </pre
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import { gql } from "graphql-tag";
+
+export default {
+  apollo: {
+    posts: gql`
+      query getPosts {
+        posts {
+          data {
+            id
+            attributes {
+              Header
+              creator
+              customers {
+                data {
+                  attributes {
+                    CustomerName
+                    Status
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  },
+};
+</script>
---
+++ pages/posts.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-for="item in posts.data" :key="item.id">
+            <pre>{{item.attributes.Header}}</pre>
+            <pre>{{item.attributes.creator}}</pre>
+        </div>
+    </div>
+</template>
+
+<script>
+import { gql } from 'graphql-tag'
+
+export default {
+    apollo : {
+        posts : gql `
+       query getPosts{
+            posts {
+         data {
+         id
+         attributes {
+        Header
+        creator
+      }
+    }
+  }
+}`
+    }
+}
+</script>
